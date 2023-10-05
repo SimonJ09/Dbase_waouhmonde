c2afd3ec21601017fb6a9017d07dbb9adf82d90c
jude <jude-simon.agohoundje@epitech.eu>
2023-10-05 19:42:21 +0100

    waouh monde 1

--- Database/start.sh
+++ Database/start.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
+pip install --upgrade pip
+pip install pandas
+pip install gunicorn
+python manage.py collectstatic
 python manage.py makemigrations
 python manage.py migrate
-
-gunicorn Xage1.wsgi:application --bind 0.0.0.0:$PORT --workers 4
+gunicorn Database.wsgi:application --bind 0.0.0.0:$PORT --workers 4
